dca122150e4e31d07f3bafe4356d123e642014e9
kool44 <68624389+kool44@users.noreply.github.com>
2026-01-09 14:59:58 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+import http from "http";
+import httpProxy from "http-proxy";
+
+const proxy = httpProxy.createProxyServer({
+  target: "https://assured-home-dream.lovable.app",
+  changeOrigin: true,
+  secure: true,
+});
+
+const server = http.createServer((req, res) => {
+  proxy.web(req, res, (err) => {
+    res.writeHead(502);
+    res.end("Proxy error");
+  });
+});
+
+server.listen(process.env.PORT || 3000);
